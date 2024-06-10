9deac844680a073801bf683dd92f9ffa69475687
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-06-10 15:31:02 +0000

    Passing Handler Prop in React

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,18 @@
+import React, {useState} from 'react';
+import '../styles/Child.css';
 
+const Selection = (props) => {
+  const { applyColor } = props
+  const [backgroundcolor, setBackgroundColor] = useState({ backgroundColor: "" });
+
+  const handleClick = () => {
+    setBackgroundColor({backgroundColor: applyColor.background});
+  };
+
+  return (
+    <div className='fix-box' style={backgroundcolor} onClick={handleClick} >
+        <center>Selection</center>
+    </div>
+  )
+}
+export default Selection;
